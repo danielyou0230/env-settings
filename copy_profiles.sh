71fb003b7abff2e616c3ef0991a4bfe77abf32e6
danielyou0230 <danielyou0230@gmail.com>
2019-02-03 15:00:09 +0800

    add Sublime Text profile

--- copy_profiles.sh
+++ copy_profiles.sh
@@ -1,5 +1,10 @@
 cp ~/.vimrc ~/.vimrc_bk
 cp .vimrc ~/.vimrc
 
-mv ~/.config/karabiner/karabiner.json ~/.config/karabiner/karabiner.json_bk
+mv ~/.config/karabiner/karabiner.json ~/.config/karabiner/karabiner_bk.json
 cp karabiner.json ~/.config/karabiner/karabiner.json
+
+mv ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings \
+~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences_bk.sublime-settings
+
+cp Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
